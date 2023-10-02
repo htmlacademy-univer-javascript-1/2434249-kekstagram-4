ecbd2c0d765ed54f27bb509771cdc20b5f5fb0fa
Kirill <evtar2020@gmail.com>
2023-10-02 20:26:37 +0500

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -3,19 +3,25 @@ function correctLength(inputString, length) {
 }
 
 function isPalindrom(inputString) {
-  const stringWithoutSpaces = inputString.replace(/ /g, "").toLowerCase();
-  const reversedString = stringWithoutSpaces.split("").reverse().join("");
+  const stringWithoutSpaces = inputString.replace(/ /g, '').toLowerCase();
+  const reversedString = stringWithoutSpaces.split('').reverse().join('');
 
   return stringWithoutSpaces === reversedString ? true : false;
 }
 
 function getNumbers(inputString) {
-  let numbers = "";
-  const stringWithoutSpaces = inputString.replace(/ /g, "");
+  let numbers = '';
+  const stringWithoutSpaces = inputString.replace(/ /g, '');
 
-  for (let symbol = 0; symbol <= stringWithoutSpaces.length; symbol++)
-    if (Number.isInteger(+stringWithoutSpaces[symbol]))
+  for (let symbol = 0; symbol <= stringWithoutSpaces.length; symbol++) {
+    if (Number.isInteger(+stringWithoutSpaces[symbol])) {
       numbers += stringWithoutSpaces[symbol];
+    }
+  }
 
   return parseInt(numbers);
 }
+
+correctLength('qwerty', 10);
+isPalindrom('Лёша на полке клопа нашёл ');
+getNumbers('2023 год');
