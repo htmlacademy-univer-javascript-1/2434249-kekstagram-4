989f6c4564de1b61b2c53c2ada4cdc2a43bacab8
Kirill <evtar2020@gmail.com>
2023-10-02 13:42:46 +0500

    Нужно больше функций

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+function correctLength(inputString, length) {
+  return inputString.length <= length ? true : false;
+}
+
+function isPalindrom(inputString) {
+  const stringWithoutSpaces = inputString.replace(/ /g, "").toLowerCase();
+  const reversedString = stringWithoutSpaces.split("").reverse().join("");
+
+  return stringWithoutSpaces === reversedString ? true : false;
+}
+
+function getNumbers(inputString) {
+  let numbers = "";
+  const stringWithoutSpaces = inputString.replace(/ /g, "");
+
+  for (let symbol = 0; symbol <= stringWithoutSpaces.length; symbol++)
+    if (Number.isInteger(+stringWithoutSpaces[symbol]))
+      numbers += stringWithoutSpaces[symbol];
+
+  return parseInt(numbers);
+}
