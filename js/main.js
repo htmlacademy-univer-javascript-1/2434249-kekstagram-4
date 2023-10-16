17e2a7c28b7ba92d6a3d644ab5f92a82264e7b52
Kirill <evtar2020@gmail.com>
2023-10-16 14:10:17 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+const PHOTO_ID_MIN = 1;
+const PHOTO_ID_MAX = 25;
+const PICTIRE_ADDRESS_MIN = 1;
+const PICTIRE_ADDRESS_MAX = 25;
+const LIKES_COUNT_MIN = 15;
+const LIKES_COUNT_MAX = 200;
+const AVATAR_PATH_MIN = 1;
+const AVATAR_PATH_MAX = 6;
+const PHOTO_ARRAY_LENGTH = 25;
+
+const USER_NAMES = [
+  'Alexsander',
+  'Pavel',
+  'Maria',
+  'Anna',
+  'Kiril',
+  'Victor'
+];
+const USER_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
 //Генерация случайного числа
 const getRandomInteger = () => Math.round(Math.random() * 1000);
 
@@ -10,7 +37,7 @@ const getRandomRangeInteger = (a, b) => {
 };
 
 //Генерация числа без повторений
-function createRandomIdFromRangeGenerator (min, max) {
+const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
   return function () {
@@ -24,31 +51,14 @@ function createRandomIdFromRangeGenerator (min, max) {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
+};
 
 //Выбор случайного элемента из массива
 const getRandomArrayElement = (elements) => elements[getRandomRangeInteger(0, elements.length - 1)];
 
-const names = [
-  'Alexsander',
-  'Pavel',
-  'Maria',
-  'Anna',
-  'Kiril',
-  'Victor'
-];
-const messages = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const generatePhotoId = createRandomIdFromRangeGenerator(1, 25);
-const generatePictureAdderss = createRandomIdFromRangeGenerator(1, 25);
-const generateLikesCount = createRandomIdFromRangeGenerator(15, 200);
+const generatePhotoId = createRandomIdFromRangeGenerator(PHOTO_ID_MIN, PHOTO_ID_MAX);
+const generatePictureAdderss = createRandomIdFromRangeGenerator(PICTIRE_ADDRESS_MIN, PICTIRE_ADDRESS_MAX);
+const generateLikesCount = createRandomIdFromRangeGenerator(LIKES_COUNT_MIN, LIKES_COUNT_MAX);
 
 //Создание объекта
 const createPhotoInfo = () => ({
@@ -58,12 +68,12 @@ const createPhotoInfo = () => ({
   likes: generateLikesCount(),
   comments: {
     id: getRandomInteger(),
-    avatar: `img/avatar-${getRandomRangeInteger(1, 6)}.svg`,
-    message: getRandomArrayElement(messages),
-    name: getRandomArrayElement(names)
+    avatar: `img/avatar-${getRandomRangeInteger(AVATAR_PATH_MIN, AVATAR_PATH_MAX)}.svg`,
+    message: getRandomArrayElement(USER_MESSAGES),
+    name: getRandomArrayElement(USER_NAMES)
   }
 });
 
 const createArrayPhotos = (photosCount) => Array.from({length: photosCount}, createPhotoInfo);
 
-createArrayPhotos(25);
+createArrayPhotos(PHOTO_ARRAY_LENGTH);
