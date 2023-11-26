253f7a63acf20e56e0cb77d94a378093c81d4adb
Kirill <evtar2020@gmail.com>
2023-11-27 01:06:11 +0500

    Открывается и закрывается (часть 2)

--- js/big-picture-render.js
+++ js/big-picture-render.js
@@ -5,7 +5,7 @@ export const bigPicture = document.querySelector('.big-picture');
 const closeElement = document.querySelector('.big-picture__cancel');
 const body = document.querySelector('body');
 const countComments = document.querySelector('.social__comment-count');
-const commentsLoader = document.querySelector('.comments-loader');
+const commentLoader = document.querySelector('.social__comments-loader');
 
 export const renderBigPicture = (pictures, data) => {
   pictures.forEach((picture, index) =>  {
@@ -20,13 +20,48 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
+function loadNewComments(number, list) {
+  const startIndex = number.start;
+  if (number.start < list.length - 5) {
+    number.start += 5;
+  } else {
+    number.start += list.length - number.start;
+    commentLoader.classList.add('hidden');
+  }
+
+  for (let i = startIndex; i < number.start; i++) {
+    bigPicture.querySelector('.social__comments').appendChild(list[i]);
+  }
+
+  bigPicture.querySelector('.current-comments-count').textContent = number.start;
+
+  if (number.start === list.length) {
+    number.start = 5;
+  }
+}
+
 function openBigPicture(picture) {
   bigPicture.classList.remove('hidden');
-  countComments.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   body.classList.add('modal-open');
 
   createBigPictureData(picture);
+
+  if (picture.comments.length > 5) {
+    const number = {start: 5};
+    const fullCommentList = bigPicture.querySelector('.social__comments').querySelectorAll('.social__comment');
+
+    bigPicture.querySelector('.social__comments').innerHTML = '';
+
+    for (let i = 0; i < number.start; i++) {
+      bigPicture.querySelector('.social__comments').appendChild(fullCommentList[i]);
+    }
+
+    commentLoader.addEventListener('click', () => loadNewComments(number, fullCommentList));
+  } else {
+    document.querySelector('.social__comment-count').classList.add('hidden');
+    commentLoader.classList.add('hidden');
+    document.querySelector('.social__comments-loader').removeEventListener('click', loadNewComments);
+  }
 
   document.addEventListener('keydown', onDocumentKeydown);
   closeElement.addEventListener('click', closeBigPicture);
@@ -35,11 +70,14 @@ function openBigPicture(picture) {
 function closeBigPicture() {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
-  countComments.classList.remove('hidden');
-  commentsLoader.classList.remove('hidden');
 
   cleanBigPictureData();
 
+  countComments.classList.remove('hidden');
+  commentLoader.classList.remove('hidden');
+
   document.removeEventListener('keydown', onDocumentKeydown);
   closeElement.removeEventListener('click', closeBigPicture);
+  //Почему-то не удалается обработчик...
+  commentLoader.removeEventListener('click', loadNewComments);
 }
