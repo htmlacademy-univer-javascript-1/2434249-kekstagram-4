daa270fcd91aff86337aa1f161ce6f9bb2928be1
Kirill <evtar2020@gmail.com>
2023-11-27 01:19:59 +0500

    Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -2,7 +2,5 @@ import {renderingImages, generatePictures} from './pictures-render.js';
 import {renderBigPicture} from './big-picture-render.js';
 
 renderingImages();
-
 const picturesArray = document.querySelectorAll('.picture');
-
 renderBigPicture(picturesArray, generatePictures);
