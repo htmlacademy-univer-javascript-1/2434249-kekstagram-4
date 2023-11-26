ba2bebc4843dc4cb57aec3d020bda46387d5e4d2
Kirill <evtar2020@gmail.com>
2023-11-27 01:19:00 +0500

    Открывается и закрывается

--- js/big-picture-data.js
+++ js/big-picture-data.js
@@ -1,18 +1,13 @@
 import {bigPicture} from './big-picture-render.js';
 
 export const createBigPictureData = (picture) => {
-  bigPicture.querySelector('.social__comment-count').parentNode.removeChild(bigPicture.querySelector('.social__comment-count'));
-  const commentCount = `<div class="social__comment-count"><span class="current-comments-count">${5}</span> из <span class="comments-count">${125}</span> комментариев</div>`;
-  bigPicture.querySelector('.social__comments').insertAdjacentHTML( 'beforebegin', commentCount);
-
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
-  bigPicture.querySelector('.social__comments').innerHTML = '';
 
   for (const comments of picture.comments) {
-    const comment = `<li class="social__comment">
+    const commentsList = `<li class="social__comment">
   <img
       class="social__picture"
       src="${comments.avatar}"
@@ -20,7 +15,7 @@ export const createBigPictureData = (picture) => {
       width="35" height="35">
   <p class="social__text">${comments.message}</p>
 </li>`;
-    bigPicture.querySelector('.social__comments').insertAdjacentHTML( 'afterbegin', comment);
+    bigPicture.querySelector('.social__comments').insertAdjacentHTML( 'beforeend', commentsList );
   }
 };
 
@@ -28,7 +23,6 @@ export const cleanBigPictureData = () => {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = '';
   bigPicture.querySelector('.likes-count').textContent = '';
   bigPicture.querySelector('.comments-count').textContent = '';
-  bigPicture.querySelector('.current-comments-count').textContent = '';
   bigPicture.querySelector('.social__caption').textContent = '';
   bigPicture.querySelector('.social__comments').innerHTML = '';
 };
