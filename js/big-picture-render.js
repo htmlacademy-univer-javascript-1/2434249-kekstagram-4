cb6d03b1862afebd26535b3bc15299a4ad42b11b
Kirill <evtar2020@gmail.com>
2023-11-28 14:47:12 +0500

    Открывается и закрывается

--- js/big-picture-render.js
+++ js/big-picture-render.js
@@ -1,12 +1,15 @@
 import {isEscapeKey} from './util.js';
-import {createBigPictureData, cleanBigPictureData} from './big-picture-data.js';
 
 export const bigPicture = document.querySelector('.big-picture');
-const closeElement = document.querySelector('.big-picture__cancel');
+const closeBtn = document.querySelector('.big-picture__cancel');
 const body = document.querySelector('body');
 const countComments = document.querySelector('.social__comment-count');
 const commentLoader = document.querySelector('.social__comments-loader');
-// let COMMENT_STEP = 5;
+const COMMENT_STEP = 5;
+let currentCommentCount = 5;
+
+let comments = 0;
+let fullCommentList = 0;
 
 export const renderBigPicture = (pictures, data) => {
   pictures.forEach((picture, index) =>  {
@@ -25,50 +28,81 @@ const onCloseBtnClick = () => {
   closeBigPicture();
 };
 
-// function loadNewComments(list) {
-//   const startIndex = COMMENT_STEP;
-//   if (COMMENT_STEP < list.length - 5) {
-//     COMMENT_STEP += 5;
-//   } else {
-//     COMMENT_STEP += list.length - COMMENT_STEP;
-//     commentLoader.classList.add('hidden');
-//   }
+const createBigPictureData = (picture) => {
+  bigPicture.querySelector('.social__comment-count').parentNode.removeChild(bigPicture.querySelector('.social__comment-count'));
+  const commentCount = `<div class="social__comment-count"><span class="current-comments-count">${5}</span> из <span class="comments-count">${125}</span> комментариев</div>`;
+  bigPicture.querySelector('.social__comments').insertAdjacentHTML( 'beforebegin', commentCount);
 
-//   for (let i = startIndex; i < COMMENT_STEP; i++) {
-//     bigPicture.querySelector('.social__comments').appendChild(list[i]);
-//   }
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = picture.url;
+  bigPicture.querySelector('.likes-count').textContent = picture.likes;
+  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = picture.description;
+  bigPicture.querySelector('.social__comments').innerHTML = '';
 
-//   bigPicture.querySelector('.current-comments-count').textContent = COMMENT_STEP;
+  picture.comments.forEach((comment) => {
+    const commentFragment = `<li class="social__comment">
+    <img
+        class="social__picture"
+        src="${comment.avatar}"
+        alt="${comment.name}"
+        width="35" height="35">
+    <p class="social__text">${comment.message}</p>
+  </li>`;
+    bigPicture.querySelector('.social__comments').insertAdjacentHTML( 'afterbegin', commentFragment);
+  });
+};
 
-//   if (COMMENT_STEP === list.length) {
-//     COMMENT_STEP = 5;
-//   }
-// }
+const cleanBigPictureData = () => {
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = '';
+  bigPicture.querySelector('.likes-count').textContent = '';
+  bigPicture.querySelector('.comments-count').textContent = '';
+  bigPicture.querySelector('.current-comments-count').textContent = '';
+  bigPicture.querySelector('.social__caption').textContent = '';
+  bigPicture.querySelector('.social__comments').innerHTML = '';
+};
+
+function loadNewComments() {
+  if (currentCommentCount < fullCommentList.length - COMMENT_STEP) {
+    currentCommentCount += COMMENT_STEP;
+  } else {
+    currentCommentCount += fullCommentList.length - currentCommentCount;
+    commentLoader.classList.add('hidden');
+  }
+
+  for (let i = 0; i < currentCommentCount; i++) {
+    bigPicture.querySelector('.social__comments').appendChild(fullCommentList[i]);
+  }
+
+  bigPicture.querySelector('.current-comments-count').textContent = currentCommentCount;
+
+  if (currentCommentCount === fullCommentList.length) {
+    currentCommentCount = COMMENT_STEP;
+  }
+}
 
 function openBigPicture(picture) {
+  comments = picture.comments;
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
 
   createBigPictureData(picture);
+  if (comments.length > 5) {
+    fullCommentList = bigPicture.querySelector('.social__comments').querySelectorAll('.social__comment');
 
-  // if (picture.comments.length > 5) {
-  //   const fullCommentList = bigPicture.querySelector('.social__comments').querySelectorAll('.social__comment');
+    bigPicture.querySelector('.social__comments').innerHTML = '';
 
-  //   bigPicture.querySelector('.social__comments').innerHTML = '';
+    for (let i = 0; i < COMMENT_STEP; i++) {
+      bigPicture.querySelector('.social__comments').appendChild(fullCommentList[i]);
+    }
 
-  //   for (let i = 0; i < COMMENT_STEP; i++) {
-  //     bigPicture.querySelector('.social__comments').appendChild(fullCommentList[i]);
-  //   }
-
-  //   commentLoader.addEventListener('click', () => loadNewComments(fullCommentList));
-  // } else {
-  //   document.querySelector('.social__comment-count').classList.add('hidden');
-  //   commentLoader.classList.add('hidden');
-  //   document.querySelector('.social__comments-loader').removeEventListener('click', loadNewComments);
-  // }
+    commentLoader.addEventListener('click', loadNewComments);
+  } else {
+    document.querySelector('.social__comment-count').classList.add('hidden');
+    commentLoader.classList.add('hidden');
+  }
 
   document.addEventListener('keydown', onDocumentKeydown);
-  closeElement.addEventListener('click', onCloseBtnClick);
+  closeBtn.addEventListener('click', onCloseBtnClick);
 }
 
 function closeBigPicture() {
@@ -77,11 +111,12 @@ function closeBigPicture() {
 
   cleanBigPictureData();
 
+  fullCommentList= 0;
+
   countComments.classList.remove('hidden');
   commentLoader.classList.remove('hidden');
 
   document.removeEventListener('keydown', onDocumentKeydown);
-  closeElement.removeEventListener('click', onCloseBtnClick);
-  // //Почему-то не удалается обработчик...
-  // commentLoader.removeEventListener('click', loadNewComments);
+  closeBtn.removeEventListener('click', onCloseBtnClick);
+  commentLoader.removeEventListener('click', loadNewComments);
 }
