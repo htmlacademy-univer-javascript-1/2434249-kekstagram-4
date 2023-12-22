9ea1b61d9f41c1f13989e9cdae5da370991ed88c
Kirill <evtar2020@gmail.com>
2023-12-22 21:24:13 +0500

    Перламутровые пуговицы

--- js/util.js
+++ js/util.js
@@ -1,7 +1,5 @@
-//Генерация случайного числа
 export const getRandomInteger = () => Math.round(Math.random() * 1000);
 
-//Генерация случайного числа из диапазаона
 export const getRandomRangeInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -9,7 +7,6 @@ export const getRandomRangeInteger = (a, b) => {
   return Math.floor(result);
 };
 
-//Выбор случайного элемента из массива
 export const getRandomArrayElement = (elements) => elements[getRandomRangeInteger(0, elements.length - 1)];
 
 export const isEscapeKey = (evt) => evt.key === 'Escape';
